db734fc77f03a5d93f861ade1d6bc01d95d13431
fujimoto-naoya <fujimotonaoya@team-lab.com>
2019-11-11 11:49:12 +0900

    ルーティングの変更
    api/v1/***でアクセスできるように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,18 @@
 Rails.application.routes.draw do
 
-
   root :to => "oauth_test#index"
-
 
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
   }
 
-
-
   get 'oauth_test/show' => 'oauth_test#show'
-
-
   get 'oauth_test/error' => 'oauth_test#error'
 
-
-
-
+  namespace 'api' do
+    namespace 'v1' do
+      resource :items
+    end
+  end
 end
